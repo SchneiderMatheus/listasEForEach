dbaf1b162e9bcb5c2d5d5d540d1f7dccf1b38bbe
Matheus <matheuswschneider@gmail.com>
2026-06-25 23:41:23 -0300

    First Commit

--- Program1/src/App.java
+++ Program1/src/App.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        List <String>list = new ArrayList<>();
+        Locale.setDefault(Locale.US);
+
     }
 }
