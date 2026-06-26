05c6f24fdb7816d5b2cfa5089e8f37d7dc445ad3
Matheus <matheuswschneider@gmail.com>
2026-06-26 18:28:39 -0300

    Desafio

---
+++ Program1/src/App3.java
@@ -0,0 +1,9 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+    }
+}
